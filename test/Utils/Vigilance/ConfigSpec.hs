f8598cd62b16462fe1fcb01a78de89864b0aa0ff
Michael Xavier <michael@michaelxavier.net>
2013-08-09 13:12:28 -0700

    config testing wip

--- test/Utils/Vigilance/ConfigSpec.hs
+++ test/Utils/Vigilance/ConfigSpec.hs
@@ -1,10 +1,17 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Utils.Vigilance.ConfigSpec (spec) where
 
+import ClassyPrelude
 import SpecHelper
 
 import Utils.Vigilance.Config
 
 spec :: Spec
 spec = do
-  describe "configNotifiers" $ do
-    it "always generates a logger" $ pending
+  describe "loadConfig" $ do
+    it "always generates a logger" $ do
+      "empty_config.conf" `shouldParseConfig` mempty
+
+p `shouldParseConfig` cfg = loadConfig path `shouldReturn` cfg
+  where path = show ("test" </> "fixtures" </> p)
